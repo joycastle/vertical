1e5d873c0003761bba2dffab81f47ca45a5c8223
LevinLFX <levin@joycastle.mobi>
2022-05-17 15:43:00 +0800

    done

---
+++ component.go
@@ -0,0 +1,5 @@
+package config
+
+var C_Log map[string]LogConf = make(map[string]LogConf)
+var C_Mysql map[string]MysqlConf = make(map[string]MysqlConf)
+var C_Redis map[string]RedisConf = make(map[string]RedisConf)
