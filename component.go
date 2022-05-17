a4a62bfc178f2808ad2b272d47aaa69d9bcdc051
LevinLFX <levin@joycastle.mobi>
2022-05-17 17:08:18 +0800

    done

--- component.go
+++
@@ -1,5 +0,0 @@
-package vertical
-
-var C_Log map[string]LogConf = make(map[string]LogConf)
-var C_Mysql map[string]MysqlConf = make(map[string]MysqlConf)
-var C_Redis map[string]RedisConf = make(map[string]RedisConf)
