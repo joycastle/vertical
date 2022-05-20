1f5b0b46da6002db7f2860fb39f047ed0b75113e
LevinLFX <levin@joycastle.mobi>
2022-05-20 16:10:53 +0800

    done

--- connector/mysql.go
+++ connector/mysql.go
@@ -90,6 +90,36 @@ func GetMysqlConn(config MysqlConf) (*gorm.DB, error) {
 		sqlDb.SetMaxIdleConns(config.MaxIdle)
 		sqlDb.SetMaxOpenConns(config.MaxOpen)
 		sqlDb.SetConnMaxLifetime(config.MaxLifeTime)
+
+		//stats monitor
+		go func() {
+			//type DBStats struct {
+			//	MaxOpenConnections int // Maximum number of open connections to the database.
+
+			// Pool Status
+			//	OpenConnections int // The number of established connections both in use and idle.
+			//	InUse           int // The number of connections currently in use.
+			//	Idle            int // The number of idle connections.
+
+			// Counters
+			//	WaitCount         int64         // The total number of connections waited for.
+			//	WaitDuration      time.Duration // The total time blocked waiting for a new connection.
+			//	MaxIdleClosed     int64         // The total number of connections closed due to SetMaxIdleConns.
+			//	MaxIdleTimeClosed int64         // The total number of connections closed due to SetConnMaxIdleTime.
+			//	MaxLifetimeClosed int64         // The total number of connections closed due to SetConnMaxLifetime.
+			//}
+			for {
+				time.Sleep(time.Second * 10)
+				stat := sqlDb.Stats()
+				infos := fmt.Sprintf("MYSQL Connection open:%d, inUse:%d, idle:%d, waitCount:%d, waitDuration:%v",
+					stat.OpenConnections,
+					stat.InUse,
+					stat.Idle,
+					stat.WaitCount,
+					stat.WaitDuration)
+				log.GetLogger("monitor").Infof(infos)
+			}
+		}()
 	}
 
 	return gdb, nil
